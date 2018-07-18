9df43939baee8fcd5012cbd215cb36f8d288adb4
Colin Larkin <36335073+cjal920@users.noreply.github.com>
2018-07-18 12:26:26 -0400

    Update liri.js

--- liri.js
+++ liri.js
@@ -2,10 +2,3 @@ require("dotenv").config();
 
 var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
-
-var keys = require('./keys.js');
-var request = require('request');
-var twitter = require('twitter');
-var spotify = require('spotify');
-var client = new twitter(keys.twitterKeys);
-var fs = require('fs');
